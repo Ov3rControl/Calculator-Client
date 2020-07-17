0788c96c7b5ff8811bfe2d7adaae95be4a67c3bd
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-07-17 19:19:47 +0200

    :sparkles: Add Two Numbers Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CalculatorService } from './calculator.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'calculator-client';
+
+  sum$: Object;
+
+  constructor(private calculatorService: CalculatorService) {}
+
+  addNumbers(num1: number, num2: number) {
+    this.calculatorService
+      .addNumbers(num1, num2)
+      .subscribe((res) => (this.sum$ = res));
+  }
 }
--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculatorService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  addNumbers(num1: number, num2: number) {
+    const ADD_NUMBERS_URL = `http://localhost:3000/api/v1/add/${num1}/${num2}`;
+    return this.http.get(ADD_NUMBERS_URL);
+  }
 }
